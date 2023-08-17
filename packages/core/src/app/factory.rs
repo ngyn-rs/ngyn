e6528aef3d146b8f84cdf3d4767b46c8eb31cfdc
elcharitas <jonathanirhodia@gmail.com>
2023-08-17 15:40:56 +0100

    feat: create `RustleModule` trait

--- packages/core/src/app/factory.rs
+++ packages/core/src/app/factory.rs
@@ -1,11 +1,9 @@
+use rustle_shared::RustleModule;
+
 pub struct RustleFactory {}
 
-pub trait Creatable {
-    fn new() -> Self;
-}
-
 impl RustleFactory {
-    pub fn create<T: Creatable>() -> tide::Server<()> {
+    pub fn create<T: RustleModule>() -> tide::Server<()> {
         let app = tide::new();
         // app.with(module);
         app
---
+++ packages/shared/src/traits/module.rs
@@ -0,0 +1,3 @@
+pub trait RustleModule {
+    fn new() -> Self;
+}
