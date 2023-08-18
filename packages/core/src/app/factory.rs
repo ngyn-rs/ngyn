623782bd64dc803b131d59b029a88bf8a74bcd8e
elcharitas <jonathanirhodia@gmail.com>
2023-08-18 20:08:31 +0100

    fix: module trait typings

--- packages/core/src/app/factory.rs
+++ packages/core/src/app/factory.rs
@@ -1,10 +1,17 @@
+use std::any::Any;
+
 use rustle_shared::RustleModule;
 
 pub struct RustleFactory {}
 
 impl RustleFactory {
-    pub fn create<T: RustleModule>() -> tide::Server<()> {
+    pub fn create<T: RustleModule<C, P>, C: Any, P: Any>() -> tide::Server<()> {
         let app = tide::new();
+        let module = T::new();
+        module.get_controllers();
+        // .iter().for_each(|controller| {
+        //     // app.at(controller.path()).get(controller.handler());
+        // });
         // app.with(module);
         app
     }
--- packages/shared/src/traits/module.rs
+++ packages/shared/src/traits/module.rs
@@ -1,3 +1,14 @@
-pub trait RustleModule {
+/// `RustleModule` is a trait that defines the basic structure of a module in Rustle.
+/// It requires two generic parameters: `C` for the controller type, and `P` for the provider type.
+pub trait RustleModule<C, P> {
+    /// Creates a new instance of the module.
     fn new() -> Self;
+
+    /// Returns the controllers of the module.
+    /// It is expected that this method will be overridden in the implementation of the module.
+    fn get_controllers(&self) -> C;
+
+    /// Returns the providers of the module.
+    /// It is expected that this method will be overridden in the implementation of the module.
+    fn get_providers(&self) -> P;
 }
