b4e9a7c925b73199b5bdd9deebb2ac515fa77bae
elcharitas <jonathanirhodia@gmail.com>
2023-08-18 05:18:41 +0100

    chore: rename module to follow convention

--- examples/basic_app/src/main.rs
+++ examples/basic_app/src/main.rs
@@ -1,11 +1,11 @@
 mod modules;
 
-use modules::sample::sample_module::Sample;
+use modules::sample::sample_module::SampleModule;
 use rustle_core::RustleFactory;
 
 #[async_std::main]
 async fn main() -> tide::Result<()> {
-    let app = RustleFactory::create::<Sample>();
+    let app = RustleFactory::create::<SampleModule>();
 
     app.listen("127.0.0.1:8080").await?;
 
--- examples/basic_app/src/modules/sample/sample_module.rs
+++ examples/basic_app/src/modules/sample/sample_module.rs
@@ -3,6 +3,6 @@ use rustle_core::module;
 use super::sample_service::SampleService;
 
 #[module]
-pub struct Sample {
+pub struct SampleModule {
     sample_service: SampleService,
 }
