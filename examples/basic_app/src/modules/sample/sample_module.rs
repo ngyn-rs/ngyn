725825be6155bf033ff019bc9abdce68e54d0cee
elcharitas <jonathanirhodia@gmail.com>
2023-08-18 23:04:55 +0100

    fix: inject and load services

--- examples/basic_app/src/modules/sample/sample_module.rs
+++ examples/basic_app/src/modules/sample/sample_module.rs
@@ -1,4 +1,8 @@
 use rustle_core::module;
 
+use super::sample_service::SampleService;
+
 #[module]
-pub struct SampleModule;
+pub struct SampleModule {
+    providers: Vec<std::sync::Arc<SampleService>>,
+}
