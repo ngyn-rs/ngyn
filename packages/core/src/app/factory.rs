ae49bb32632c2af19758dae993ad1b895c3a400f
elcharitas <jonathanirhodia@gmail.com>
2023-08-19 22:41:49 +0100

    chore: improve controller routes discovery

--- packages/core/src/app/factory.rs
+++ packages/core/src/app/factory.rs
@@ -4,12 +4,18 @@ pub struct RustleFactory {}
 
 impl RustleFactory {
     pub fn create<AppModule: RustleModule>() -> tide::Server<()> {
-        let app = tide::new();
+        let mut app = tide::new();
         let module = AppModule::new();
-        module.get_controllers().iter().for_each(|_controller| {
-            // app.at(controller.path()).get(controller.handler());
+        module.get_controllers().iter().for_each(|controller| {
+            for (path, http_method, _handler) in controller.routes() {
+                match http_method {
+                    // TODO: use an enum for HTTP methods
+                    "get" => app.at(path).get(|_| async move { Ok("") }),
+                    // TODO: Add other HTTP methods as needed
+                    _ => panic!("Unsupported HTTP method"),
+                };
+            }
         });
-        // app.with(module);
         app
     }
 }
