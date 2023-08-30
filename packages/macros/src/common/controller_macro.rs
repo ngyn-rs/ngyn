801c536b6dd3b1fe0a7c4a04f92bf735496b4dc9
elcharitas <jonathanirhodia@gmail.com>
2023-08-30 07:44:23 +0100

    feat: new attempt at route discovery

--- packages/macros/src/common/controller_macro.rs
+++ packages/macros/src/common/controller_macro.rs
@@ -3,9 +3,60 @@ use quote::quote;
 
 use crate::utils::handle_macro::handle_macro;
 
+fn extract_route(method: &syn::ImplItemFn) -> Option<String> {
+    let route_attr = method.attrs.iter().find(|attr| {
+        if let Ok(meta) = attr.parse_args() {
+            if let syn::Meta::Path(path) = meta {
+                return path.is_ident("get")
+                    || path.is_ident("post")
+                    || path.is_ident("put")
+                    || path.is_ident("delete");
+            }
+        }
+        false
+    });
+
+    if let Some(attr) = route_attr {
+        if let Ok(route_meta) = attr.parse_args() {
+            if let syn::Meta::Path(path) = route_meta {
+                return Some(path.segments.last().unwrap().ident.to_string());
+            }
+        }
+    }
+
+    None
+}
+
+fn find_impl_block(input: &syn::DeriveInput) -> Option<syn::ItemImpl> {
+    if let syn::Data::Struct(_) = input.data {
+        for item in &input.attrs {
+            if let Ok(meta) = item.parse_args::<syn::Meta>() {
+                if meta.path().is_ident("controller") {
+                    if let syn::Item::Impl(impl_block) = syn::Item::from(input.clone()) {
+                        return Some(impl_block);
+                    }
+                }
+            }
+        }
+    }
+    None
+}
+
 pub fn controller_macro(_args: TokenStream, input: TokenStream) -> TokenStream {
     let input = syn::parse_macro_input!(input as syn::DeriveInput);
-    let (ident, types, keys) = handle_macro(input);
+    let (ident, types, keys) = handle_macro(input.clone());
+
+    let mut routes = Vec::new();
+
+    if let Some(impl_block) = find_impl_block(&input) {
+        for item in impl_block.items {
+            if let syn::ImplItem::Fn(method) = item {
+                if let Some(route) = extract_route(&method) {
+                    routes.push(route);
+                }
+            }
+        }
+    }
 
     let fields: Vec<_> = types
         .iter()
@@ -42,7 +93,29 @@ pub fn controller_macro(_args: TokenStream, input: TokenStream) -> TokenStream {
                         + Sync,
                 >,
             )> {
-                vec![]
+                // build a vec of all methods available to this controller
+                let mut routes: Vec<(
+                    &str,
+                    &str,
+                    Box<
+                        dyn Fn(rustle_core::RustleRequest, rustle_core::RustleResponse) -> rustle_core::RustleResponse
+                            + Send
+                            + Sync,
+                    >,
+                )> = vec![];
+
+                // add all routes to the vec
+                #(
+                    routes.push((
+                        #routes,
+                        "GET",
+                        Box::new(move |req, res| {
+                            #ident::#routes(self.clone(), req, res)
+                        })
+                    ));
+                )*
+
+                routes
             }
         }
 
