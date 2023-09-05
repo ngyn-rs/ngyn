2088f5a048de27e8b4c1be962965b4ed93104aa8
elcharitas <jonathanirhodia@gmail.com>
2023-09-05 18:22:22 +0100

    fix(sample): force consistent return type

--- examples/basic_app/src/modules/sample/sample_controller.rs
+++ examples/basic_app/src/modules/sample/sample_controller.rs
@@ -9,13 +9,13 @@ pub struct SampleController {
 
 impl SampleController {
     #[get("/")]
-    fn say_hello(self, _req: RustleRequest, res: RustleResponse) {
+    fn say_hello(self, _req: RustleRequest, res: RustleResponse) -> RustleResponse {
         self.sample_service.say_hello();
         res.body("Hello, Rustle!")
     }
 
     #[get(["/bye", "/goodbye"])]
-    fn say_goodbye(self, _req: RustleRequest, res: RustleResponse) {
+    fn say_goodbye(self, _req: RustleRequest, res: RustleResponse) -> RustleResponse {
         println!("Goodbye!");
         res
     }
