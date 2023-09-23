0cd3421f120f8b1028c7fb9fe5d1d030f4132d6c
elcharitas <jonathanirhodia@gmail.com>
2023-09-23 08:53:22 +0100

    fix: state explicit app

--- examples/weather_app/src/main.rs
+++ examples/weather_app/src/main.rs
@@ -2,12 +2,12 @@ mod modules;
 
 use dotenv::dotenv;
 use modules::AppModule;
-use ngyn::{NgynFactory, Result};
+use ngyn::{server::NgynApplication, NgynFactory, Result};
 
 #[tokio::main]
 async fn main() -> Result<()> {
     dotenv().ok();
-    let app = NgynFactory::create::<AppModule>();
+    let app = NgynFactory::<NgynApplication>::create::<AppModule>();
 
     println!("Starting server at http://127.0.0.1:8080");
 
