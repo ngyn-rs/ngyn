d97a52a07a319c5fc37e237e428ebd673b32e778
elcharitas <jonathanirhodia@gmail.com>
2023-10-02 22:27:16 +0100

    fix: route macro arg parser

--- crates/macros/src/common/route_macro.rs
+++ crates/macros/src/common/route_macro.rs
@@ -1,7 +1,7 @@
 use ngyn_shared::path_enum::Path;
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::ItemFn;
+use syn::{parse_macro_input, ItemFn, Signature};
 
 use crate::utils::str_to_ident;
 
@@ -10,77 +10,74 @@ struct Args {
     http_method: String,
 }
 
+impl syn::parse::Parse for Args {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        let http_method = input.parse::<syn::LitStr>()?.value().to_uppercase();
+
+        if !["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS"]
+            .contains(&http_method.as_str())
+        {
+            panic!("Unsupported HTTP method: {}", http_method)
+        } else {
+            input.parse::<syn::Token![,]>()?;
+        }
+
+        let path = if input.peek(syn::LitStr) {
+            let path = input.parse::<syn::LitStr>()?;
+            Some(Path::Single(path.value()))
+        } else if input.peek(syn::token::Bracket) {
+            let content;
+            syn::bracketed!(content in input);
+            let mut paths = Vec::new();
+            while !content.is_empty() {
+                let path = content.parse::<syn::LitStr>()?;
+                paths.push(path.value());
+                if !content.is_empty() {
+                    content.parse::<syn::Token![,]>()?;
+                }
+            }
+            Some(Path::Multiple(paths))
+        } else {
+            None
+        };
+
+        Ok(Args { path, http_method })
+    }
+}
+
 pub fn route_macro(args: TokenStream, raw_input: TokenStream) -> TokenStream {
-    let input = match syn::parse::<ItemFn>(raw_input) {
+    let ItemFn {
+        sig,
+        block,
+        vis,
+        attrs,
+    } = match syn::parse::<ItemFn>(raw_input) {
         Ok(input) => input,
         Err(err) => {
             let error_message = err.to_string();
             panic!("failed to parse method: {}", error_message)
         }
     };
-
-    let args = {
-        let input_str = args.to_string();
-
-        let (path, http_method) = if input_str.starts_with('"') {
-            // the input_str should be of this structure: `"GET", "/path"` or `"GET", ["/path1", "/path2"]`
-            // match the first argument to see if it's a valid HTTP method
-            let mut input_str = input_str.trim_matches('"').split(',');
-            let http_method = input_str
-                .next()
-                .unwrap()
-                .to_string()
-                .trim_matches('"')
-                .to_uppercase();
-
-            // panic if the HTTP method is invalid
-            if !["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS"]
-                .contains(&http_method.as_str())
-            {
-                panic!("invalid HTTP method: {}", http_method)
-            }
-
-            // match everything else as the path or paths
-            let route_path = input_str
-                .skip(0)
-                .map(|s| s.trim())
-                .collect::<Vec<_>>()
-                .join(",");
-            let path = if route_path.starts_with('[') {
-                let paths: Vec<_> = route_path
-                    .trim_matches(|p| p == '[' || p == ']')
-                    .split(',')
-                    .map(|p| p.trim_matches('"').to_string())
-                    .collect();
-                Some(Path::Multiple(paths))
-            } else if route_path.is_empty() {
-                Some(Path::Single("/".to_string()))
-            } else {
-                Some(Path::Single(route_path.trim_matches('"').to_string()))
-            };
-            (path, http_method)
-        } else {
-            panic!("route expects at least one argument")
-        };
-
-        Args { path, http_method }
-    };
+    let args = parse_macro_input!(args as Args);
 
     let path = args.path.unwrap();
     let http_method = args.http_method;
 
-    let ident = &input.sig.ident;
-    let fn_vis = &input.vis;
-    let fn_async = &input.sig.asyncness;
-    let inputs = &input.sig.inputs;
-    let output = match &input.sig.output {
+    let Signature {
+        asyncness,
+        ident,
+        inputs,
+        output,
+        fn_token,
+        ..
+    } = sig;
+
+    let output = match output {
         syn::ReturnType::Default => quote! { ngyn::NgynResponse },
         syn::ReturnType::Type(_, ty) => quote! { #ty },
     };
-    let block = &input.block;
 
     let mut expanded_methods: Vec<_> = Vec::new();
-
     match path {
         Path::Multiple(paths) => {
             for path in paths {
@@ -101,7 +98,8 @@ pub fn route_macro(args: TokenStream, raw_input: TokenStream) -> TokenStream {
     let register_ident = str_to_ident(format!("register_{}", ident));
 
     let expanded = quote! {
-        #fn_vis #fn_async fn #ident(#inputs) -> #output #block
+        #(#attrs)*
+        #vis #asyncness #fn_token #ident(#inputs) -> #output #block
 
         fn #register_ident(&mut self) {
             #(#expanded_methods)*
