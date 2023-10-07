00489b27abe228ee03f7263e0385fb84a156d079
Jonathan Irhodia <jonathanirhodia@gmail.com>
2023-10-07 03:10:51 +0100

    fix: use ureq in weather api sample (#30)

--- examples/weather_app/src/modules/weather/weather_repository.rs
+++ examples/weather_app/src/modules/weather/weather_repository.rs
@@ -1,5 +1,5 @@
 use ngyn::injectable;
-use std::{collections::HashMap, env, error::Error};
+use std::env;
 
 #[injectable]
 pub struct WeatherRepository;
@@ -13,16 +13,12 @@ impl WeatherRepository {
         )
     }
 
-    async fn send_request(&self, url: &str) -> Result<HashMap<String, String>, Box<dyn Error>> {
-        let response = reqwest::get(url)
-            .await?
-            .json::<HashMap<String, String>>()
-            .await?;
-
+    async fn send_request(&self, url: &str) -> Result<String, ureq::Error> {
+        let response = ureq::get(url).call()?.into_string()?;
         Ok(response)
     }
 
-    pub async fn get_location_current_weather(&self, location: &str) -> HashMap<String, String> {
+    pub async fn get_location_current_weather(&self, location: &str) -> String {
         println!("Getting weather for {}", location);
         let url = self.build_url("current", location);
         println!("Sending request to {}", url);
