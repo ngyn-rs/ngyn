94634fd4f81105427ca150d42b824a4bf5c9ff4e
elcharitas <jonathanirhodia@gmail.com>
2023-10-21 13:31:31 +0100

    feat: support mutable handlers for fluent api

--- crates/core/src/server/engine.rs
+++ crates/core/src/server/engine.rs
@@ -1,14 +1,14 @@
 use ngyn_shared::{HttpMethod, NgynRequest, NgynResponse};
 
 pub trait Handler: Sync + Send + 'static {
-    fn handle(&self, req: NgynRequest, res: NgynResponse) -> NgynResponse;
+    fn handle(&self, req: &NgynRequest, res: &mut NgynResponse);
 }
 
 impl<F> Handler for F
 where
-    F: Fn(NgynRequest, NgynResponse) -> NgynResponse + Send + Sync + 'static,
+    F: Fn(&NgynRequest, &mut NgynResponse) -> () + Send + Sync + 'static,
 {
-    fn handle(&self, req: NgynRequest, res: NgynResponse) -> NgynResponse {
+    fn handle(&self, req: &NgynRequest, res: &mut NgynResponse) {
         self(req, res)
     }
 }
