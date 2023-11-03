0b36fc1c93f1372aa4ea5e3fa6b89966094051bb
Jonathan Irhodia <jonathanirhodia@gmail.com>
2023-11-03 21:48:34 +0100

    chore: bump version to 0.2.4 (#41)

--- examples/vercel_app/src/modules/sample/sample_controller.rs
+++ examples/vercel_app/src/modules/sample/sample_controller.rs
@@ -9,13 +9,13 @@ pub struct SampleController {
 
 impl SampleController {
     #[get("/hello")]
-    fn say_hello(self, _req: &NgynRequest, res: &NgynResponse) -> NgynResponse {
+    fn say_hello(self, _req: &NgynRequest, res: &mut NgynResponse) -> NgynResponse {
         self.sample_service.say_hello();
         res.body("Hello, Ngyn!").clone()
     }
 
     #[get(["/bye", "/goodbye"])]
-    fn say_goodbye(self, _req: &NgynRequest, res: &NgynResponse) -> NgynResponse {
+    fn say_goodbye(self, _req: &NgynRequest, res: &mut NgynResponse) -> NgynResponse {
         println!("Goodbye!");
         res.clone()
     }
