a9753ddb4000e492b14e7bc5d5508659ac586019
elcharitas <jonathanirhodia@gmail.com>
2023-12-26 17:36:07 +0100

    feat: support grouped routes gates

--- crates/macros/src/common/check_macro.rs
+++ crates/macros/src/common/check_macro.rs
@@ -1,6 +1,8 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{ItemFn, Signature};
+use syn::{ItemFn, ItemImpl};
+
+use crate::utils::str_to_ident;
 
 struct CheckArgs {
     gates: Vec<syn::Path>,
@@ -23,29 +25,57 @@ impl syn::parse::Parse for CheckArgs {
     }
 }
 
-pub fn check_macro(args: TokenStream, input: TokenStream) -> TokenStream {
+pub fn check_impl_macro(impl_item: ItemImpl, args: TokenStream) -> TokenStream {
+    let mut new_items = Vec::new();
+    let attrs = syn::Attribute {
+        pound_token: Default::default(),
+        style: syn::AttrStyle::Outer,
+        bracket_token: Default::default(),
+        meta: syn::Meta::List(syn::MetaList {
+            path: syn::Path::from(str_to_ident("check".to_string())),
+            delimiter: syn::MacroDelimiter::Paren(Default::default()),
+            tokens: args.into(),
+        }),
+    };
+
+    for item in impl_item.items {
+        let new_item = match item {
+            syn::ImplItem::Fn(mut method) => {
+                if method.attrs.clone().into_iter().any(|attr| {
+                    attr.path().is_ident("route")
+                        || attr.path().is_ident("get")
+                        || attr.path().is_ident("post")
+                        || attr.path().is_ident("put")
+                        || attr.path().is_ident("delete")
+                        || attr.path().is_ident("patch")
+                        || attr.path().is_ident("head")
+                        || attr.path().is_ident("options")
+                }) {
+                    method.attrs.push(attrs.clone());
+                }
+                syn::ImplItem::Fn(method)
+            }
+            _ => item,
+        };
+        new_items.push(new_item);
+    }
+    let new_impl = syn::Item::Impl(syn::ItemImpl {
+        items: new_items,
+        ..impl_item
+    });
+    quote::quote!(#new_impl).into()
+}
+
+pub fn check_fn_macro(args: TokenStream, input: TokenStream) -> TokenStream {
+    let mut input = syn::parse_macro_input!(input as ItemFn);
     let CheckArgs { gates } = syn::parse_macro_input!(args as CheckArgs);
-    let ItemFn {
-        attrs,
-        vis,
-        sig,
-        block,
-    } = syn::parse_macro_input!(input as ItemFn);
 
-    let Signature {
-        ident,
-        inputs,
-        output,
-        fn_token,
-        ..
-    } = sig;
-
-    let req = match inputs.iter().nth(1) {
+    let req = match input.sig.inputs.iter().nth(1) {
         Some(syn::FnArg::Typed(pat_type)) => &pat_type.pat,
         _ => panic!("Expected a valid request parameter"),
     };
 
-    let res = match inputs.iter().nth(2) {
+    let res = match input.sig.inputs.iter().nth(2) {
         Some(syn::FnArg::Typed(pat_type)) => &pat_type.pat,
         _ => panic!("Expected a valid response parameter"),
     };
@@ -63,12 +93,16 @@ pub fn check_macro(args: TokenStream, input: TokenStream) -> TokenStream {
         }
     });
 
-    let check_fn = quote! {
-        #(#attrs)*
-        #vis async #fn_token #ident(#inputs) #output {
+    input.block.stmts.insert(
+        0,
+        syn::parse2::<syn::Stmt>(quote! {
             #(#gates)*
-            #block
-        }
+        })
+        .unwrap(),
+    );
+
+    let check_fn = quote! {
+        #input
     };
 
     check_fn.into()
