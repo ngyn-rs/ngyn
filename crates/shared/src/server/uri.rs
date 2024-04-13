29a4c3a3a0980b9a5ec8ead3b3e41d8215245fb9
elcharitas <jonathanirhodia@gmail.com>
2024-04-13 19:47:53 +0100

    chore: clippy fixes

--- crates/shared/src/server/uri.rs
+++ crates/shared/src/server/uri.rs
@@ -8,7 +8,7 @@ fn path_to_regex(path: &str) -> regex::Regex {
     let path_b = format!("^{}$", path);
     let path_b = path_b.replace('/', "\\/");
     let path_b = path_b.replace('*', "(.*)");
-    let path_b = path_b.replace('<', "(?P<").replace(">", ">[^/]+)");
+    let path_b = path_b.replace('<', "(?P<").replace('>', ">[^/]+)");
     regex::Regex::new(&path_b).unwrap()
 }
 
@@ -23,13 +23,13 @@ impl ToParts for Uri {
             return (false, Vec::new());
         }
 
-        let path_b = parts_path.split("/").collect::<Vec<&str>>();
+        let path_b = parts_path.split('/').collect::<Vec<&str>>();
         let path_a = uri_path.split('/').collect::<Vec<&str>>();
 
         let mut named_matches_with_values = Vec::new();
 
         path_b.iter().for_each(|part| {
-            if part.contains("<") {
+            if part.contains('<') {
                 let path_r = path_to_regex(part);
                 let name = path_r.capture_names().nth(1).unwrap().unwrap();
                 let path_b_pos = path_b.iter().position(|&x| &x == part).unwrap();
