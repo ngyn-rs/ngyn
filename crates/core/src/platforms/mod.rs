723326131348ea966d39f213339cbc58fd7d0338
elcharitas <jonathanirhodia@gmail.com>
2024-04-13 12:15:32 +0100

    chore(feature): drop support for tide

--- crates/core/src/platforms/mod.rs
+++ crates/core/src/platforms/mod.rs
@@ -1,11 +1,7 @@
 pub(crate) mod hyper;
-#[cfg(feature = "tide")]
-pub(crate) mod tide;
 #[cfg(feature = "vercel")]
 pub(crate) mod vercel;
 
 pub use hyper::*;
-#[cfg(feature = "tide")]
-pub use tide::*;
 #[cfg(feature = "vercel")]
 pub use vercel::*;
