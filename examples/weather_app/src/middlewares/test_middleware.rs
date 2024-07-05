bc10d1e7545325fded2e7ad19a423e7647f8fb62
Jonathan Irhodia <jonathanirhodia@gmail.com>
2024-07-05 12:25:47 +0100

    Update test_middleware.rs

--- examples/weather_app/src/middlewares/test_middleware.rs
+++ examples/weather_app/src/middlewares/test_middleware.rs
@@ -3,7 +3,7 @@ use ngyn::prelude::*;
 #[injectable]
 pub struct TestMiddleware;
 
-impl NgynMiddleware for TestMiddleware {
+impl NgynMiddleware<'_> for TestMiddleware {
     fn handle(&self, _cx: &mut NgynContext, _response: &mut NgynResponse) {
         println!("middleware works");
     }
