be6c1c4509c12774f4bf373a756b34b50805fa68
elcharitas <jonathanirhodia@gmail.com>
2024-08-04 16:01:40 +0100

    chore(vercel-platform): improve chunk exploration

--- crates/vercel/src/lib.rs
+++ crates/vercel/src/lib.rs
@@ -22,10 +22,15 @@ impl VercelApplication {
         let body = {
             let mut buf = Vec::new();
             let frame = body.frame().await;
-            if frame.is_some() {
-                let chunk = frame.unwrap().unwrap();
-                let d = chunk.data_ref().unwrap();
-                buf.extend_from_slice(d.to_vec().as_slice());
+
+            match frame {
+                Some(frame) => {
+                    if let Ok(chunk) = frame {
+                        let d = chunk.data_ref().unwrap();
+                        buf.extend_from_slice(d.to_vec().as_slice());
+                    }
+                }
+                None => {}
             }
             Body::from(buf)
         };
