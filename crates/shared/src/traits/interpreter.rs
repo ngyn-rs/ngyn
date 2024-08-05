a6765a8ee8e21875d3313a06bf5cc003d997af29
Jonathan Irhodia <jonathanirhodia@gmail.com>
2024-08-05 19:06:05 +0100

    feat(shared): support for response interpreters (#134)
    * feat(shared): route response interpreters
    
    * add response interpreting
    
    * support async interpretation
    
    * reassign response
    
    * remove test parse
    
    * cleanups

---
+++ crates/shared/src/traits/interpreter.rs
@@ -0,0 +1,10 @@
+use crate::server::NgynResponse;
+
+/// NgynInterpreter is used to interpret a response.
+///
+/// Sometimes, a response may need to be interpreted before it is sent back to the client.
+/// This trait provides a way to do that.
+#[async_trait::async_trait]
+pub trait NgynInterpreter: Send + Sync {
+    async fn interpret(&self, res: &mut NgynResponse) -> NgynResponse;
+}
