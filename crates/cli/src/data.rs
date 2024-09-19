580820c8d5c62859398b341a8627a8d8ecf1dbee
elcharitas <jonathanirhodia@gmail.com>
2024-09-19 02:03:30 +0100

    remove unused dep

--- crates/cli/src/data.rs
+++ crates/cli/src/data.rs
@@ -1,14 +1,6 @@
 // use anyhow::anyhow;
 // use anyhow::Result as AnyResult;
-use std::collections::HashMap;
-
 use ramhorns::Content;
-use serde_derive::{Deserialize, Serialize};
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct Definitions {
-    pub providers: HashMap<String, String>,
-}
 
 pub struct CmdExit {
     pub code: exitcode::ExitCode,
