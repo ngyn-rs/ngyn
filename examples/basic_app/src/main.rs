ca52f319bfb6b5a7fce218eacc704914aefe95e4
elcharitas <jonathanirhodia@gmail.com>
2024-12-05 14:18:57 +0100

    fix: param expand test

--- examples/basic_app/src/main.rs
+++ examples/basic_app/src/main.rs
@@ -4,10 +4,7 @@ use ngyn::prelude::*;
 async fn main() {
     let mut app = HyperApplication::default();
 
-    app.get(
-        "/author",
-        handler(|_cx: &mut NgynContext| "Ngyn is created by @elcharitas."),
-    );
+    app.get("/author", handler(|_cx| "Ngyn is created by @elcharitas."));
 
     println!("Starting server at http://127.0.0.1:8080");
 
