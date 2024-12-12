eed0e0c62810f90196c6833beb4935218a32b94e
elcharitas <jonathanirhodia@gmail.com>
2024-12-12 22:13:19 +0100

    chore: swizzle out components

---
+++ sites/docs/src/theme/Navbar/Logo/index.tsx
@@ -0,0 +1,11 @@
+import Logo from "@theme/Logo";
+
+export default function NavbarLogo(): JSX.Element {
+	return (
+		<Logo
+			className="navbar__brand"
+			imageClassName="navbar__logo"
+			titleClassName="navbar__title text--truncate"
+		/>
+	);
+}
