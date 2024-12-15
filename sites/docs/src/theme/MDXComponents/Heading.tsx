c6bbd942a5b1e2c7e3403d39a74f95c02054a9df
elcharitas <jonathanirhodia@gmail.com>
2024-12-15 19:18:30 +0100

    chore: headings styles

--- sites/docs/src/theme/MDXComponents/Heading.tsx
+++ sites/docs/src/theme/MDXComponents/Heading.tsx
@@ -2,7 +2,10 @@ import { Heading } from "@site/src/components/Heading";
 import type { Props } from "@theme/MDXComponents/Heading";
 
 export default function MDXHeading({ as, ...props }: Props): JSX.Element {
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	const _cls = "text-5xl text-4xl text-3xl text-2xl text-xl";
 	return (
+		// @ts-expect-error types
 		<Heading level={Number.parseInt(as.replace("h", "")) as never} {...props} />
 	);
 }
