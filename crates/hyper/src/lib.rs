581a4c87a6592f7331788911c07c0e12577f7d33
elcharitas <jonathanirhodia@gmail.com>
2024-12-21 08:46:00 +0100

    feat: implement graceful shutdown

--- crates/hyper/src/lib.rs
+++ crates/hyper/src/lib.rs
@@ -4,6 +4,8 @@ use hyper::server::conn::http1;
 use hyper::{service::service_fn, Request};
 use hyper_util::rt::TokioIo;
 use ngyn_shared::core::engine::{NgynHttpPlatform, PlatformData};
+use ngyn_shared::server::NgynResponse;
+use std::future::Future;
 use std::sync::Arc;
 use tokio::net::TcpListener;
 
@@ -34,44 +36,81 @@ impl HyperApplication {
         address: A,
     ) -> Result<(), std::io::Error> {
         let server = TcpListener::bind(address).await?;
-
         let data = Arc::new(self.data);
 
-        let service = service_fn(move |req: Request<Incoming>| {
-            let data = data.clone();
-            async move {
-                let (parts, mut body) = req.into_parts();
-                let body = {
-                    let mut buf = Vec::new();
-                    if let Some(frame) = body.frame().await {
-                        let chunk = frame?.into_data();
-                        if let Ok(bytes) = chunk {
-                            buf.extend_from_slice(&bytes)
-                        }
-                    }
-                    buf
-                };
-                let req = Request::from_parts(parts, body);
-                let res = data.respond(req).await;
-
-                Ok::<_, hyper::Error>(res)
-            }
-        });
         let http = http1::Builder::new();
+        let graceful = hyper_util::server::graceful::GracefulShutdown::new();
+        // when this signal completes, start shutdown
+        let mut signal = std::pin::pin!(shutdown_signal());
 
         loop {
+            let data = data.clone();
             tokio::select! {
                 Ok((stream, _)) = server.accept() => {
                     let io = TokioIo::new(stream);
-                    let conn = http.serve_connection(io, service.clone());
+                    let conn = http.serve_connection(io, service_fn(move |req| hyper_service(data.clone(), req)));
+                    let handle = graceful.watch(conn);
 
                     tokio::task::spawn(async move {
-                        if let Err(e) = conn.await {
-                            eprintln!("server error: {}", e);
+                        if let Err(e) = handle.await {
+                            eprintln!("server connection error: {}", e);
                         }
                     });
                 }
+                _ = &mut signal => {
+                    eprintln!("graceful shutdown signal received");
+                    // stop the accept loop
+                    break;
+                }
+                else => continue, // continue waiting for the next signal or connection
             }
         }
+
+        tokio::select! {
+            _ = graceful.shutdown() => {
+                eprintln!("all connections gracefully closed");
+            },
+            _ = tokio::time::sleep(std::time::Duration::from_secs(10)) => {
+                eprintln!("timed out wait for all connections to close");
+            }
+        }
+
+        Ok(())
     }
+}
+
+fn hyper_service(
+    data: Arc<PlatformData>,
+    req: Request<Incoming>,
+) -> impl Future<Output = Result<NgynResponse, hyper::Error>> {
+    async move {
+        let (parts, mut body) = req.into_parts();
+        let body = {
+            let mut buf = Vec::new();
+            // TODO: change this approach. It's not efficient.
+            loop {
+                if let Some(frame) = body.frame().await {
+                    let chunk = frame?.into_data();
+                    if let Ok(bytes) = chunk {
+                        buf.extend_from_slice(&bytes);
+                    } else {
+                        break;
+                    }
+                } else {
+                    break;
+                }
+            }
+            buf
+        };
+        let req = Request::from_parts(parts, body);
+        let res = data.respond(req).await;
+
+        Ok::<_, hyper::Error>(res)
+    }
+}
+
+async fn shutdown_signal() {
+    tokio::signal::ctrl_c()
+        .await
+        .expect("failed to listen for signal");
 }
