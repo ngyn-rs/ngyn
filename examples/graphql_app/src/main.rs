63b47ed7e0e963d54b669bdb2d9addaa88580838
Jonathan Irhodia <jonathanirhodia@gmail.com>
2025-01-12 20:07:42 +0100

    fix: sync handler returns (#246)

--- examples/graphql_app/src/main.rs
+++ examples/graphql_app/src/main.rs
@@ -8,6 +8,11 @@ use ngyn_hyper::HyperApplication;
 use std::sync::Arc;
 
 #[handler]
+fn home() -> &'static str {
+    "<html><body>You can access the GraphQL playground at <a href='/playground'>/playground</a> or the GraphiQL interface at <a href='/graphiql'>/graphiql</a>.</body></html>"
+}
+
+#[handler]
 async fn handle_graphql(req: NgynRequest) -> String {
     let db = Arc::new(Database::new());
     let root_node = Arc::new(RootNode::new(
@@ -19,30 +24,31 @@ async fn handle_graphql(req: NgynRequest) -> String {
     graphql_res.body().as_str().to_owned()
 }
 
+#[handler]
+async fn handle_graphiql() -> String {
+    let graphiql_res = graphiql("/graphql", None).await;
+    graphiql_res.body().as_str().to_owned()
+}
+
+#[handler]
+async fn handle_playground() -> String {
+    let playground_res = playground("/graphql", None).await;
+    playground_res.body().as_str().to_owned()
+}
+
 #[tokio::main]
 async fn main() {
     let mut app = HyperApplication::default();
 
-    app.get("/", handler(|_c| "<html><body>You can access the GraphQL playground at <a href='/playground'>/playground</a> or the GraphiQL interface at <a href='/graphiql'>/graphiql</a>.</body></html>"));
+    app.get("/", home);
 
-    app.get(
-        "/playground",
-        async_handler(|_c| async {
-            let playground_res = playground("/graphql", None).await;
-            playground_res.body().as_str().to_owned()
-        }),
-    );
+    app.get("/playground", async_wrap(handle_playground));
 
-    app.get(
-        "/graphiql",
-        async_handler(|_c| async {
-            let graphiql_res = graphiql("/graphql", None).await;
-            graphiql_res.body().as_str().to_owned()
-        }),
-    );
+    app.get("/graphiql", async_wrap(handle_graphiql));
 
     app.any("/graphql", async_wrap(handle_graphql));
 
     println!("Listening on http://127.0.0.1:8080");
+
     let _ = app.listen("127.0.0.1:8080").await;
 }
